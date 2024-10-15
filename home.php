ac385fd8c573c0eac2b41e9685f8f3259b755b30
Guilhermecubo <guilherme@cubotecnologiabr.com.br>
2024-10-15 15:13:47 -0300

    Update home.php

--- home.php
+++ home.php
@@ -14,6 +14,33 @@ if (isset($_SESSION['welcome_message'])) {
 
 echo '<a href="?logout">Fazer logout!</a>';
 
+function carregarUsuarios() {
+    $arquivo = 'usuarios.json';
+    if (file_exists($arquivo)) {
+        $dadosJson = file_get_contents($arquivo);
+        return json_decode($dadosJson, true);
+    }
+    return [];
+}
+
+
+$usuarios = carregarUsuarios();
+
+if ($usuarios) {
+    echo '<h3>Usuários Cadastrados:</h3>';
+    echo '<table border="1">';
+    echo '<tr><th>Nome</th><th>Email</th></tr>';
+    foreach ($usuarios as $usuario) {
+        echo '<tr>';
+        echo '<td>' . htmlspecialchars($usuario['nome']) . '</td>';
+        echo '<td>' . htmlspecialchars($usuario['email']) . '</td>';
+        echo '</tr>';
+    }
+    echo '</table>';
+} else {
+    echo '<p>Nenhum usuário cadastrado.</p>';
+}
+
 
 if (isset($_GET['logout'])) {
     unset($_SESSION['login']);
